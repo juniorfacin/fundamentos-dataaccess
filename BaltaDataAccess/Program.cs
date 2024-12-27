e4918584e929af91317e4fab0d9589dda8923720
Junior F <jufacin@hotmail.com>
2024-12-26 21:00:35 -0300

    Initial commmit: data acces com Dapper

---
+++ BaltaDataAccess/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.Common;
+using BaltaDataAccess.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string stringConnection = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$;Encrypt=True;TrustServerCertificate=True";
+
+            using (var connection = new SqlConnection(stringConnection))
+            {
+                Console.WriteLine("Conectado");
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+                foreach (var category in categories)
+                {
+                    Console.WriteLine($"{category.Id} - {category.Title}");
+                }
+            }
+        }
+    }
+}
